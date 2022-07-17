f46a6b00874021624dd32590669531ce3fbd6c8c
Jackson Silva <cajui54@gmail.com>
2022-07-17 20:19:52 -0300

    update margin top

--- src/components/Header/styledNav.js
+++ src/components/Header/styledNav.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Nav = styled.nav`
     position: absolute;
-    margin-top: 910px;
+    margin-top: 66px;
     width: 100%;
     height: 850px;
     padding: 0;
