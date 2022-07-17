6802843b91dc6a704964ce2e2843d346f578eb93
Jackson Silva <cajui54@gmail.com>
2022-07-17 20:46:15 -0300

    up

--- src/components/Header/styledNav.js
+++ src/components/Header/styledNav.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Nav = styled.nav`
     position: absolute;
-    margin-top: 460px;
+    margin-top: 440px;
     width: 100%;
     height: 750px;
     padding: 0;
